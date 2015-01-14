94cdaab89a9fa97e2f984bf14d79a7ec60627488
JustEnki <justin.hd.elder@gmail.com>
2015-01-14 11:48:58 -0800

    Finished Dilect, now troubleshooting

--- Dialect.py
+++ Dialect.py
@@ -87,4 +87,66 @@ class OldeDialectizer(Dialectizer):
               (r'([bcdfghjklmnpqrstvwxyz])y', r'\1ee'),
               (r'([aeiou]re\b', r'\1r'),
               (r'ia[bcdfgjklmnpqrstvwxyz])', r'i\1e'),
+              (r'tion\b', r'cioun'),
+              (r'ion\b', r'ioun'),
+              (r'aid', r'ayde'),
+              (r'ai', r'ey'),
+              (r'ay\b', r'y'),
+              (r'ay', r'ey'),
+              (r'ant', r'aunt'),
+              (r'ea', r'ee'),
+              (r'oa', r'oo'),
+              (r'ue', r'e'),
+              (r'oe', r'o'),
+              (r'ou', r'ow'),
+              (r'ow', r'ou'),
+              (r'\bhe', r'hi'),
+              (r've\b', r'veth'),
+              (r'se\b', r'e'),
+              (r"'s\b", r'es'),
+              (r'ic\b', r'ick'),
+              (r'ics\b', r'icc'),
+              (r'ical\b', r'ick'),
+              (r'tle\b', r'til'),
+              (r'll\b', r'l'),
+              (r'ould\b', r'olde'),
+              (r'own\b', r'oune'),
+              (r'un\b', r'onne'),
+              (r'rry\b', r'rye'),
+              (r'est\b', r'este'),
+              (r'pt\b', r'pte'),
+              (r'th\b', r'the'),
+              (r'ch\b', r'che'),
+              (r'ss\b',r'sse'),
+              (r'([wybdp])\b', r'\1e'),
+              (r'([rnt])\b',r'\1\1e'),
+              (r'from', r'fro'),
+              (r'when', r'whan'))
               
+def translate(url, dialectName = "chef"):
+  """fetch URL and translate using dialectName
+  
+  dialect in ("chef", "fudd", "olde")"""
+  import urllib
+  sock = urllib.urlopen(url)
+  htmlSource = sock.read()
+  sock.close()
+  parserName = "%sDialectizer" % dialectName.capitalize()
+  parserClass = globals()[parserName]
+  parser = parserClass()
+  parser.feed(htmlSource)
+  parser.close()
+  return parser.output()
+
+def test(url):
+  """test all dialects against URL"""
+  for dialect in ("chef", "fudd", "olde"):
+    outfile = "%s.html" % dialect
+    fsock = open(outfile, "wb")
+    fsock.write(translate(url, dialect))
+    fsock.close()
+    import webbrowser
+    webbrowser.open_new(outfile)
+      
+if __name__=="__main__":
+  test("http://diveintopython.org/odbchelper_list.html")
